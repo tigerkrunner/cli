ba3fe658eb2151af1e256834af2b64740bb1485f
Will Murphy <wmurphy@pivotal.io>
2018-10-31 11:18:25 -0700

    WIP fix up actor-level unit test
    [#126256725]
    
    Signed-off-by: Jen Spinney <jspinney@pivotal.io>

--- actor/v2action/router_group_test.go
+++ actor/v2action/router_group_test.go
@@ -7,47 +7,38 @@ import (
 	. "code.cloudfoundry.org/cli/actor/v2action"
 	"code.cloudfoundry.org/cli/actor/v2action/v2actionfakes"
 	"code.cloudfoundry.org/cli/api/router"
-	"code.cloudfoundry.org/cli/api/router/routererror"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 )
 
 var _ = Describe("Router Group Actions", func() {
 	var (
-		actor                     *Actor
-		fakeCloudControllerClient = new(v2actionfakes.FakeCloudControllerClient)
-		fakeRouterClient          *v2actionfakes.FakeRouterClient
+		actor            *Actor
+		fakeRouterClient *v2actionfakes.FakeRouterClient
 	)
 
 	BeforeEach(func() {
 		fakeRouterClient = new(v2actionfakes.FakeRouterClient)
-		actor = NewActor(fakeCloudControllerClient, nil, nil)
+		actor = NewActor(nil, nil, nil)
 	})
 
 	Describe("GetRouterGroupByName", func() {
 		var (
-			routerGroupName string
-			routerGroup     RouterGroup
-			err             error
+			routerGroup RouterGroup
+			executeErr  error
 		)
 
 		JustBeforeEach(func() {
-			routerGroup, err = actor.GetRouterGroupByName(routerGroupName, fakeRouterClient)
+			routerGroup, executeErr = actor.GetRouterGroupByName("some-router-group-name", fakeRouterClient)
 		})
 
-		When("the router group does not exist", func() {
+		When("the request succeeds, but the router group list is empty", func() {
 			BeforeEach(func() {
-				routerGroupName = "some-router-group"
-				fakeRouterClient.GetRouterGroupsByNameReturns([]router.RouterGroup{}, routererror.ErrorResponse{
-					Message:    "Router Group 'some-router-group' not found",
-					StatusCode: 404,
-					Name:       "ResourceNotFoundError",
-				})
+				fakeRouterClient.GetRouterGroupsByNameReturns([]router.RouterGroup{}, nil)
 			})
 
 			It("should return an error", func() {
-				Expect(err).To(HaveOccurred())
-				Expect(err).To(MatchError(actionerror.RouterGroupNotFoundError{Name: routerGroupName}))
+				Expect(executeErr).To(MatchError(actionerror.RouterGroupNotFoundError{Name: "some-router-group-name"}))
 				Expect(routerGroup).To(Equal(RouterGroup{}))
 				Expect(fakeRouterClient.GetRouterGroupsByNameCallCount()).To(Equal(1))
 			})
@@ -55,26 +46,25 @@ var _ = Describe("Router Group Actions", func() {
 
 		When("the router group exists", func() {
 			BeforeEach(func() {
-				routerGroupName = "default-tcp"
-				fakeRouterClient.GetRouterGroupsByNameReturns([]router.RouterGroup{router.RouterGroup{Name: routerGroupName}}, nil)
+				fakeRouterClient.GetRouterGroupsByNameReturns(
+					[]router.RouterGroup{router.RouterGroup{Name: "some-router-group-name"}},
+					nil)
 			})
 
 			It("should return the router group and not an error", func() {
-				Expect(err).ToNot(HaveOccurred())
-				Expect(routerGroup).To(Equal(RouterGroup{Name: routerGroupName}))
+				Expect(executeErr).ToNot(HaveOccurred())
+				Expect(routerGroup).To(Equal(RouterGroup{Name: "some-router-group-name"}))
 				Expect(fakeRouterClient.GetRouterGroupsByNameCallCount()).To(Equal(1))
 			})
 		})
 
 		When("the router client returns an error", func() {
 			BeforeEach(func() {
-				routerGroupName = "default-tcp"
 				fakeRouterClient.GetRouterGroupsByNameReturns([]router.RouterGroup{}, errors.New("The request failed"))
 			})
 
 			It("should return an error", func() {
-				Expect(err).To(HaveOccurred())
-				Expect(err).To(MatchError("The request failed"))
+				Expect(executeErr).To(MatchError("The request failed"))
 				Expect(fakeRouterClient.GetRouterGroupsByNameCallCount()).To(Equal(1))
 			})
 		})
